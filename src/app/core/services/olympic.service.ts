cfeb7af39983e82439eeed5befb65ca61a09c7ed
Lucien Mathieu <lucien.mathieu972@gmail.com>
2025-02-27 15:51:10 +0100

    feat : adding the types for OlympicCountry and Participation

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -2,18 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { OlympicCountry } from '../models/Olympic';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympics$ = new BehaviorSubject<OlympicCountry[] | undefined | null>(
+    undefined
+  );
 
   constructor(private http: HttpClient) {}
 
   loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+    return this.http.get<OlympicCountry[]>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
       catchError((error, caught) => {
         // TODO: improve error handling
